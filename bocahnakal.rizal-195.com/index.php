c14baa9bc85361ad71d5349ff668a2a05b898a53
rejalejale <79826693+rejalejale@users.noreply.github.com>
2021-04-28 19:55:42 +0700

    Update index.php

--- bocahnakal.rizal-195.com/index.php
+++ bocahnakal.rizal-195.com/index.php
@@ -83,9 +83,9 @@
                          <form role="form" class="header-form">
                             
                          
-                             <div class="form-group">
-                                 <a href="backend.php"><button type="submit" class="btn btn-primary btn-3x"><a href="backend.php">Login as Admin</a> <span> <i class="fa fa-angle-double-right"></i> </span></button></a>
-                             </div>
+                             <a href="backend.php"><div class="form-group">
+                                 <button type="submit" class="btn btn-primary btn-3x"><a href="backend.php">Login as Admin</a> <span> <i class="fa fa-angle-double-right"></i> </span></button>
+                             </div></a>
                          </form>
                        </div>
                         
